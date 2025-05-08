5eebbf6dbe677321ff74035fc4bb1cfec71a5ab3
faizan5zanahmad <135510351+faizan5zanahmad@users.noreply.github.com>
2025-05-08 17:21:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "jenkins-demo"
+  }
